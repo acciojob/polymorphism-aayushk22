6b46bcef413697ab5f1fdda8c545b76f23915be9
aayushk22 <ayushkumar22.ak@gmail.com>
2023-08-09 00:32:08 +0530

    3rd Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -11,15 +11,15 @@ public class Main {
     public static class Product {
 
         public int product(int x, int y) {
-            return 0;
+
         }
 
         public int product(int x, int y, int z) {
-            return 0;
+
         }
 
         public double product(double x, double y) {
-            return 0;
+
         }
     }
 }
