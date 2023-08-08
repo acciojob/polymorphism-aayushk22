633e2aadb48c4d63bfd20d4466a3ce3f4f4511af
aayushk22 <ayushkumar22.ak@gmail.com>
2023-08-09 00:17:18 +0530

    1st Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(0,0);
+        p.product(0,0,0);
+        p.product(1,1);
+    }
 }
---
+++ src/main/java/com/driver/Product.java
@@ -0,0 +1,16 @@
+package com.driver;
+
+public class Product {
+
+    public int product(int x, int y) {
+        return 0;
+    }
+
+    public int product(int x, int y, int z) {
+        return 0;
+    }
+
+    public double product(double x, double y) {
+        return 0;
+    }
+}
